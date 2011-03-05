c4a33d8e2d3e2b61093253666544f858c0a5b308
Jason Smith <jhs@couchone.com>
2011-03-05 15:05:03 +0700

    Use x_emit to do on-known queued callbacks

--- emitter.js
+++ emitter.js
@@ -13,6 +13,11 @@ function Emitter (log_label) {
 
   self.log = lib.getLogger(log_label ? ('probe_couchdb.'+log_label) : 'probe_couchdb');
 
+  // Callbacks can register for "known" states either before or after the information
+  // actually becomes known. If before, they will queue up until it is known, then run
+  // in order. Susequent "known" calls will call back immediately.
+  self.known_state = {};
+
 } // Emitter
 util.inherits(Emitter, events.EventEmitter);
 
@@ -43,12 +48,44 @@ Emitter.prototype.request = function request_wrapper(opts, callback) {
   return request.apply(self, [opts, json_body]);
 }
 
+Emitter.prototype.known = function on_known(name, cb, newval) {
+  var self = this;
+  if(!self.known_state[name])
+    self.known_state[name] = { known: false
+                             , value: undefined
+                             , callbacks: []
+                             };
+
+  var state = self.known_state[name];
+  if(cb) {
+    // Fetch the value (either call back now, or register when it is known.
+    if(! state.known)
+      state.callbacks.push(cb)
+    else
+      return cb && cb.apply(undefined, [state.value]);
+  } else {
+    // Store the value, calling back any pending callbacks.
+    state.known = true;
+    state.value = newval;
+    state.callbacks.forEach(function(cb) {
+      cb && cb.apply(undefined, [newval]);
+    })
+    state.callbacks = [];
+  }
+}
+
 // If event A triggers event B, B should wait to emit until A is finished.
 Emitter.prototype.x_emit = function push_emit() {
   var self = this
     , args = arguments;
 
-  process.nextTick(function() { self.emit.apply(self, args) })
+  process.nextTick(function() {
+    // Actually emit the event.
+    self.emit.apply(self, args)
+
+    // Also register that the value is known.
+    self.known(args[0], null, args[1]);
+  })
 }
 
 module.exports = { "Emitter" : Emitter
